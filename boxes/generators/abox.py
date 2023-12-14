371fe806aad27a350740731cdec13d84e878c552
Clement Lemeunier <clement.lemeunier@torus-actions.fr>
2023-12-14 15:11:24 +0100

    fix: Modify abox to have the perfect generator

--- boxes/generators/abox.py
+++ boxes/generators/abox.py
@@ -13,29 +13,80 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from boxes import *
+from typing import List
 from boxes.lids import LidSettings
+from boxes.default_box import DefaultBoxes
+from boxes import edges
+from boxes.edges import Settings
 
-class ABox(Boxes):
+
+class ABox(DefaultBoxes):
     """A simple Box"""
 
     description = "This box is kept simple on purpose. If you need more features have a look at the UniversalBox."
-    
+
     ui_group = "Box"
 
-    def __init__(self) -> None:
-        Boxes.__init__(self)
-        self.addSettingsArgs(edges.FingerJointSettings)
-        self.addSettingsArgs(LidSettings)
-        self.buildArgParser("x", "y", "h", "outside", "bottom_edge")
+    def __init__(
+        self,
+        x: float = 100,
+        y: float = 100,
+        h: float = 100,
+        sx: str | List = "50*3",
+        sy: str | List = "50*3",
+        sh: str | List = "50*3",
+        hi: float = 0,
+        hole_dD: str = "3.5:6.5",
+        bottom_edge: str = "h",
+        top_edge: str = "e",
+        outside: bool = True,
+        nema_mount: int = 23,
+        thickness: float = 3,
+        output: str = "box.svg",
+        format: str = "svg",
+        tabs: float = 0,
+        qr_code: bool = False,
+        debug: bool = False,
+        labels: bool = True,
+        reference: float = 100,
+        inner_corners: str = "loop",
+        burn: float = 0.1,
+    ) -> None:
+        super().__init__(
+            x,
+            y,
+            h,
+            sx,
+            sy,
+            sh,
+            hi,
+            hole_dD,
+            bottom_edge,
+            top_edge,
+            outside,
+            nema_mount,
+            thickness,
+            output,
+            format,
+            tabs,
+            qr_code,
+            debug,
+            labels,
+            reference,
+            inner_corners,
+            burn,
+        )
+        setting: Settings
+        for setting in [edges.FingerJointSettings, LidSettings]:
+            for key, arg in setting.get_arguments():
+                setattr(self, key, arg)
 
     def render(self):
         x, y, h = self.x, self.y, self.h
-        t = self.thickness
 
         t1, t2, t3, t4 = "eeee"
         b = self.edges.get(self.bottom_edge, self.edges["F"])
-        sideedge = "F" # if self.vertical_edges == "finger joints" else "h"
+        sideedge = "F"  # if self.vertical_edges == "finger joints" else "h"
 
         if self.outside:
             self.x = x = self.adjustSize(x, sideedge, sideedge)
@@ -43,20 +94,19 @@ class ABox(Boxes):
             self.h = h = self.adjustSize(h, b, t1)
 
         with self.saved_context():
-            self.rectangularWall(x, h, [b, sideedge, t1, sideedge],
-                                 ignore_widths=[1, 6], move="up")
-            self.rectangularWall(x, h, [b, sideedge, t3, sideedge],
-                                 ignore_widths=[1, 6], move="up")
+            self.rectangularWall(
+                x, h, [b, sideedge, t1, sideedge], ignore_widths=[1, 6], move="up"
+            )
+            self.rectangularWall(
+                x, h, [b, sideedge, t3, sideedge], ignore_widths=[1, 6], move="up"
+            )
 
             if self.bottom_edge != "e":
                 self.rectangularWall(x, y, "ffff", move="up")
             self.lid(x, y)
 
-        self.rectangularWall(x, h, [b, sideedge, t3, sideedge],
-                             ignore_widths=[1, 6], move="right only")
-        self.rectangularWall(y, h, [b, "f", t2, "f"],
-                             ignore_widths=[1, 6], move="up")
-        self.rectangularWall(y, h, [b, "f", t4, "f"],
-                             ignore_widths=[1, 6], move="up")
-
-
+        self.rectangularWall(
+            x, h, [b, sideedge, t3, sideedge], ignore_widths=[1, 6], move="right only"
+        )
+        self.rectangularWall(y, h, [b, "f", t2, "f"], ignore_widths=[1, 6], move="up")
+        self.rectangularWall(y, h, [b, "f", t4, "f"], ignore_widths=[1, 6], move="up")
